78373310082c300d579df5cc03df06f6438073b0
sbaviriseaty <sbaviriseaty@.ufl.edu>
2020-09-11 22:47:05 -0400

    front-end changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
-import React from 'react';
+import React, {Component} from 'react';
 import KommunicateChat from './chat';
+import {
+  Route,
+  NavLink,
+  HashRouter
+} from "react-router-dom";
+import Home from "./Home";
 
-function App() {
+class App extends Component {
+  render() {
   return (
-    <div className="App">
+    <div
+        style={{
+          display: "flex",
+          justifyContent: "left",
+          alignItems: "center",          
+          backgroundColor: "white",
+          padding: 20
+        }}
+      >
       <KommunicateChat/>
-    </div>
+    <h1>COVID Chat</h1>
+    <HashRouter>
+      <div>
+    <ul className="header">
+    <li><NavLink exact to="/">Home</NavLink></li>
+            <li><NavLink to="/stuff">Stuff</NavLink></li>
+            <li><NavLink to="/contact">Contact</NavLink></li>
+          </ul>
+          <div className="content">
+          <Route exact path="/" component={Home}/>
+          </div>
+        </div>
+          </HashRouter>
+  </div>
   );
+}
 }
 
 export default App;
