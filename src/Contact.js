6ac15b250ab4e226c58e9c1e5986156408461fad
sbaviriseaty <sbaviriseaty@.ufl.edu>
2020-09-12 22:28:11 -0400

    more text added

--- src/Contact.js
+++ src/Contact.js
@@ -1,24 +1,37 @@
 import React, { Component } from "react";
- 
+import { Container, Row, Col } from "reactstrap";
+
 class Contact extends Component {
   render() {
     return (
       <div>
-        <h2>CONTACT</h2>        
+        <h2>CONTACT</h2>
         <p>
           <ul>
-        <li>Andrei: andrei.mitrofan.99@gmail.com, Bioengineering Neuroscience, Rice University 2023</li>
+            <li>
+              Andrei: andrei.mitrofan.99@gmail.com, Bioengineering + Neuroscience,
+              Rice University '23
+            </li>
 
-<li>Eileen: esduong8921@gmail.com, Mechanical Engineering, Boston University 2023</li>
+            <li>
+              Eileen: esduong8921@gmail.com, Mechanical Engineering, Boston
+              University '23
+            </li>
 
-<li>Shubha: www.linkedin.com/in/svasisht2023, Computational Biology, University of Pennsylvania 2023</li>
+            <li>
+              Shubha: www.linkedin.com/in/svasisht2023, Computational Biology,
+              University of Pennsylvania '23
+            </li>
 
-<li>Sruthika: sbaviriseaty@ufl.edu, Biomedical Engineering, University of Florida 2022</li>
+            <li>
+              Sruthika: sbaviriseaty@ufl.edu, Biomedical Engineering, University
+              of Florida '22
+            </li>
           </ul>
         </p>
       </div>
     );
   }
 }
- 
+
 export default Contact;
