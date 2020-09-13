092a116987c71ed27fe573b7fd65ddb199e83848
sbaviriseaty <sbaviriseaty@.ufl.edu>
2020-09-12 23:33:01 -0400

    more text added

--- src/Contact.js
+++ src/Contact.js
@@ -8,22 +8,24 @@ class Contact extends Component {
         <p>
           <ul>
             <li>
-              Andrei: am164@rice.edu, Bioengineering + Neuroscience,
+            <a href="mailto:am164@rice.edu">Andrei</a>
+              : Bioengineering + Neuroscience,
               Rice University '23
             </li>
-
             <li>
-              Eileen: esduong8921@gmail.com, Mechanical Engineering, Boston
+            <a href="mailto:esduong8921@gmail.com">Eileen</a>
+              : esduong8921@gmail.com, Mechanical Engineering, Boston
               University '23
             </li>
-
             <li>
-              Shubha: www.linkedin.com/in/svasisht2023, Computational Biology,
+            <a href="www.linkedin.com/in/svasisht2023">Shubha</a>
+              : Computational Biology,
               University of Pennsylvania '23
             </li>
 
             <li>
-              Sruthika: sbaviriseaty@ufl.edu, Biomedical Engineering, University
+            <a href="mailto:sbaviriseaty@ufl.edu">Sruthika</a>
+              : Biomedical Engineering, University
               of Florida '22
             </li>
           </ul>
--- src/Stuff.js
+++ src/Stuff.js
@@ -6,13 +6,20 @@ class Stuff extends Component {
       <div>
         <h2>RESOURCES</h2>        
         <p>
+        <ol>
+          <li>
           <a href = "https://news.google.com/covid19/map?hl=en-US&gl=US&ceid=US%3Aen">Google Coronavirus Cases</a>
-          <br></br>
+          </li>
+          <li>
           <a href = "https://www.cdc.gov/coronavirus/2019-nCoV/index.html">CDC Coronavirus Disease 2019</a>
-          <br></br>
+          </li>
+          <li>
           <a href = "https://www.cdc.gov/coronavirus/2019-ncov/prevent-getting-sick/diy-cloth-face-coverings.html">CDC Guidance on Masks</a>
-          <br></br>
+          </li>
+          <li>
           <a href = "https://covid.cdc.gov/covid-data-tracker/#cases_totalcases">CDC COVID Data Tracker</a>
+          </li>
+        </ol>
         </p>
       </div>
     );
