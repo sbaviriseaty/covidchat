3ea10413b7bca9431a8e4f25701c68466bd2e8b8
sbaviriseaty <sbaviriseaty@.ufl.edu>
2020-09-12 23:44:08 -0400

    typos

--- src/Home.js
+++ src/Home.js
@@ -5,11 +5,11 @@ class Home extends Component {
     return (
       <div>
         <p><em>Disclaimer: This chat bot is not intended to replace COVID-19 medical professionals. 
-        If you are currently experience a medical emergency, please immediately consult a medical professional. 
+        If you are currently experiencing a medical emergency, please immediately consult a medical professional. 
         For more information, please read the official CDC COVID-19 guidelines.</em></p>
 
         <h2>ABOUT</h2>
-        
+
         <p>This is a hack created for PennApps 2020 which is hosted by the University of Pennsylvania. 
           This is our first hackathon! 
           It was designed as an interactive interface for the user to detail their symptoms and potential contact with other people who have tested positive for COVID-19. 
