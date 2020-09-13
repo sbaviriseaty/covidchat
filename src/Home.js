2f1a91161e740072064d90391492958a096160c3
sbaviriseaty <sbaviriseaty@.ufl.edu>
2020-09-12 21:21:19 -0400

    about page updated

--- src/Home.js
+++ src/Home.js
@@ -4,15 +4,14 @@ class Home extends Component {
   render() {
     return (
       <div>
-        <h2>HELLO</h2>
-        <p>Cras facilisis urna ornare ex volutpat, et
-        convallis erat elementum. Ut aliquam, ipsum vitae
-        gravida suscipit, metus dui bibendum est, eget rhoncus nibh
-        metus nec massa. Maecenas hendrerit laoreet augue
-        nec molestie. Cum sociis natoque penatibus et magnis
-        dis parturient montes, nascetur ridiculus mus.</p>
- 
-        <p>Duis a turpis sed lacus dapibus elementum sed eu lectus.</p>
+        <h2>ABOUT</h2>
+        <p>This is a hack created for PennApps 2020 which is hosted by the University of Pennsylvania. 
+          This is our first hackathon! 
+          It was designed as an interactive interface for the user to detail their symptoms and potential contact with other people who have tested positive for COVID-19. 
+          The chatbot then requests the location of the user in order to connect them with a COVID-19 testing site in their area. 
+          This idea was the product of 3 days of coding and collaboration between the creators, Andrei, Eileen, Shubha, and Sruthika. 
+          We hope that you enjoy the chatbot and encourage you to stay safe in these times of uncertainty. 
+          Please get tested if you or a loved one may have been exposed to COVID-19!</p>
       </div>
     );
   }
